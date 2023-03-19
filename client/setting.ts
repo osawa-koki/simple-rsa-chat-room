406f1dd5db88479d903f682684c74b14c9213483
osawa-koki <github@osawa.dev>
2023-03-19 17:02:39 +0900

    タイトル変えた。

--- client/setting.ts
+++ client/setting.ts
@@ -5,7 +5,7 @@ const setting = {
   isProd,
   basePath: Env.basePath,
   apiPath: isProd ? '' : 'http://localhost:8000',
-  title: '🐱 SignalR-Nextjs-chat 🐱',
+  title: '🙀 simple-rsa-chat-room 🙀',
   waitingTime: 1000,
 };
 
