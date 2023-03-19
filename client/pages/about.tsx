23bb5dd10cf0a00713972a053ccc74ca1b1d34d0
osawa-koki <github@osawa.dev>
2023-03-19 14:57:04 +0900

    説明ページ作った。

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -5,12 +5,10 @@ export default function AboutPage() {
   return (
     <Layout>
       <div id="About">
-        <h1>SignalR × Next.js</h1>
+        <h1>simple-rsa-chat-room</h1>
         <p className="mt-3">
-          サーバサイドはSignalR(C#)を使用してPRCによるWebSocket通信で実装し、<br />
-          クライアントサイドはNext.jsによるSSGを使用しています。<br />
-          <br />
-          Let&apos;s try SignalR × Next.js!
+          RSA暗号を用いたチャットアプリです。<br />
+          メッセージは公開鍵を用いて暗号化されて送信され、秘密鍵を有している受信者のみが復号化できます。
         </p>
       </div>
     </Layout>
